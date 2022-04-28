8576b7a8a8ab77243af88e895161956359cec9ae
s200092 <s200092@ADL-43230F>
2022-04-28 15:09:51 +0930

    Dialog stuff

---
+++ Assets/ObjectSpeech.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectSpeech
+{
+    static GameObject textBox;
+    static GameObject Text;
+
+    //Unique to object
+    public Text theText;
+
+    private TextAsset textFile;
+    public Text AAAAAA;
+    public string[] textLines;
+
+    protected int currentline;
+
+
+    void Talking()
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        currentline = Random.Range(0, 5);
+        theText.text = textLines[currentline];
+            
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+
+    void TalkingPlayer()
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        currentline = Random.Range(0, 10);
+        theText.text = textLines[currentline];
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+
+    void IdleTalking() //Only for the player
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        currentline = Random.Range(0, 9);
+        theText.text = textLines[currentline];
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
+
