3afc3ee62c9b766cc11e6175017a12bf9d545774
Pricetag_ <jakesemail098@gmail.com>
2022-04-28 15:32:51 +0930

    no message

--- Assets/ObjectSpeech.cs
+++ Assets/ObjectSpeech.cs
@@ -3,24 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ObjectSpeech : MonoBehaviour
+public class ObjectSpeech
 {
-    public Pond pond;
     static GameObject textBox;
     static GameObject Text;
 
-    //Unique to object
     public Text theText;
 
-    public TextAsset textFile;
-    public TextAsset playerText;
-    //public Text AAAAAA;
+    private TextAsset textFile;
+    public Text AAAAAA;
     public string[] textLines;
 
     protected int currentline;
 
 
-    public void Talking()
+    void Talking()
     {
         if (textFile != null)
         {
@@ -32,7 +29,7 @@ public class ObjectSpeech : MonoBehaviour
             
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            TalkingPlayer();
+            
         }
     }
 
@@ -48,7 +45,7 @@ public class ObjectSpeech : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            pond.convoEnd();
+            
         }
     }
 
@@ -67,39 +64,5 @@ public class ObjectSpeech : MonoBehaviour
             
         }
     }
-    Fish theFish;
-    public void BeginConvo(Fish fish)
-	{
-        state = ConvoState.playerTalk;
-        theFish = fish;
-	}
-
-
-    public enum ConvoState
-	{
-        no,
-        playerTalk,
-        fishTalk
-	}
-    public ConvoState state;
-	private void Update()
-	{
-		switch (state)
-		{
-            case ConvoState.no:
-                break;
-
-            case ConvoState.playerTalk:
-                //called when player is talking
-
-
-
-                break;
-
-            case ConvoState.fishTalk:
-                //called when fish is talking
-                break;
-		}
-	}
 }
 
