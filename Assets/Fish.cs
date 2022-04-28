236b89c85b21136b568ce9ebc26aa6ded12c7514
Pricetag_ <jakesemail098@gmail.com>
2022-04-28 11:17:19 +0930

    no message

---
+++ Assets/Fish.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish
+{
+    public float reelSpeed = 0;
+
+    public Fish(float rs)
+	{
+		reelSpeed = rs;
+	}
+}
---
+++ Assets/Pond.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pond : MonoBehaviour
+{
+	public float hookChance, reelSpeed;
+    public GameObject rod;
+	public GameObject hook;
+	public GameObject reelIndBg;
+	public GameObject reelIndFg;
+	Fish caughtFish;
+
+	public enum FishState
+	{
+		waiting,
+		cast,
+		reeling,
+		caught
+
+	}
+	public FishState state;
+    bool mOver = false;
+
+
+	//reel timing
+	float rTime = 0;
+	float currentRTime = 0;
+
+
+	public float reelValue = .5f;
+	Vector2 castPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rod.SetActive(false);
+		reelIndBg.SetActive(false);
+		reelIndBg.SetActive(false);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		switch (state)
+		{
+			case FishState.waiting:
+				rod.SetActive(mOver);
+				if (mOver)
+				{
+					if (Input.GetMouseButtonDown(0))
+					{
+						Cast();
+						state = FishState.cast;
+					}
+				}
+				hook.SetActive(false);
+				break;
+
+			case FishState.cast:
+				hook.SetActive(true);
+				reelIndBg.SetActive(true);
+				reelIndBg.SetActive(true);
+				if(currentRTime>rTime)
+				{
+					CatchFish();
+					state = FishState.reeling;
+				}
+				currentRTime += Time.deltaTime;
+				break;
+
+			case FishState.reeling:
+				if(reelValue > 1)
+				{
+					ReleaseFish();
+				}
+				if(reelValue < 0)
+				{
+					NetFish();
+				}
+				reelIndFg.transform.localPosition = new Vector2(reelValue - 0.5f, 0);
+				reelValue+= Time.deltaTime * caughtFish.reelSpeed;
+
+				reelValue += (Input.GetAxis("Mouse ScrollWheel"))*Time.deltaTime * reelSpeed;
+
+				break;
+		}
+
+        
+    }
+
+	void Cast()
+	{
+		castPos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+
+		currentRTime = 0;
+		rTime = Random.value * hookChance;
+		hook.transform.position = castPos;
+	}
+
+	void CatchFish()
+	{
+		caughtFish = new Fish(0.1f);
+		
+	}
+
+	void ReleaseFish()
+	{
+		state = FishState.waiting;
+		reelValue = 0.5f;
+		hook.SetActive(false);
+		reelIndBg.SetActive(false);
+		reelIndBg.SetActive(false);
+	}
+
+	void NetFish()
+	{
+		state = FishState.caught;
+		reelIndBg.SetActive(false);
+		reelIndBg.SetActive(false);
+		hook.SetActive(false);
+	}
+	
+	private void OnMouseEnter()
+	{
+        mOver = true;
+
+	}
+	private void OnMouseExit()
+	{
+	    mOver = false;
+	}
+}
